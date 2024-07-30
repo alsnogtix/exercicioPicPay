58db740ffb68640a8f36200ea9b4fb0188389bdf
alsnogtix <112113856+alsnogtix@users.noreply.github.com>
2024-07-30 10:20:26 -0300

    implementação de controller para transação, cria transação e notifica os usuários.

--- src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -16,12 +16,15 @@ public class NotificationService {
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
-        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
 
-        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
-            System.out.println("Erro ao enviar notificação");
-            throw new Exception("Serviço de notificação fora do ar");
+//        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+//
+//        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+//            System.out.println("Erro ao enviar notificação");
+//            throw new Exception("Serviço de notificação fora do ar");
+//
+//        }
 
-        }
+        System.out.println("Notificação enviada");
     }
 }
