d87afb2dd6b6bc1bb6d1106c419bb264cf745626
alsnogtix <112113856+alsnogtix@users.noreply.github.com>
2024-07-31 13:24:34 -0300

    link fornecido que retornaria a autorização não funciona, foi comentado no codigo

--- src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -18,7 +18,6 @@ public class NotificationService {
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
 //        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
-//
 //        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
 //            System.out.println("Erro ao enviar notificação");
 //            throw new Exception("Serviço de notificação fora do ar");
